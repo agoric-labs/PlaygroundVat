240a5c2572538a4fa2f98bffa6825b9cfbea6003
Brian Warner <warner@lothar.com>
2018-08-22 21:57:42 -0700

    fix non-disabled parts of test-contract.js
    The "contract test Bob lies" still fails since we don't have promise
    rejection finished.

--- test/test-contract.js
+++ test/test-contract.js
@@ -5,6 +5,14 @@ import { promisify } from 'util';
 
 function NOTtest() {}
 
+function sendCall(v, methodName, ...args) {
+  const op = {op: 'send',
+               targetSwissnum: 0,
+               methodName: methodName,
+               args: args};
+  return v.doSendOnly(JSON.stringify(op));
+}
+
 async function buildContractVat(source='../examples/contract') {
   const outputTranscript = [];
   function writeOutput(line) {
@@ -18,7 +26,7 @@ async function buildContractVat(source='../examples/contract') {
 
 test('mint test', async (t) => {
   const v = await buildContractVat('../examples/contract/contractTest');
-  const p = v.sendReceived('msg: v2->v1 {"method": "mintTest", "args": []}');
+  const p = sendCall(v, 'mintTest');
   const contractResult = await p;
   t.deepEqual(contractResult, [ 950, 50 ]);
   t.end();
@@ -26,7 +34,7 @@ test('mint test', async (t) => {
 
 test('trivial contract test', async (t) => {
   const v = await buildContractVat();
-  const p = v.sendReceived('msg: v2->v1 {"method": "trivialContractTest", "args": []}');
+  const p = sendCall(v, 'trivialContractTest');
   const contractResult = await p;
   t.equal(contractResult, 8);
   t.end();
@@ -34,7 +42,7 @@ test('trivial contract test', async (t) => {
 
 test('contract test Alice first', async (t) => {
   const v = await buildContractVat();
-  const p = v.sendReceived('msg: v2->v1 {"method": "betterContractTestAliceFirst", "args": []}');
+  const p = sendCall(v, 'betterContractTestAliceFirst');
   const contractResult = await p;
   t.equal(contractResult, 'If it fits, ware it.');
   t.end();
@@ -42,7 +50,7 @@ test('contract test Alice first', async (t) => {
 
 test('contract test Bob first', async (t) => {
   const v = await buildContractVat();
-  const p = v.sendReceived('msg: v2->v1 {"method": "betterContractTestBobFirst", "args": []}');
+  const p = sendCall(v, 'betterContractTestBobFirst');
   const contractResult = await p;
   t.deepEqual(contractResult, [7, 10]);
   t.end();
@@ -50,7 +58,7 @@ test('contract test Bob first', async (t) => {
 
 NOTtest('contract test Bob lies', async (t) => {
   const v = await buildContractVat();
-  const p = v.sendReceived('msg: v2->v1 {"method": "betterContractTestBobFirst", "args": [true]}');
+  const p = sendCall(v, 'betterContractTestBobFirst', true);
   await p.then(e => t.fail('should have broken'),
                ex => {
                  t.ok(ex.message.startsWith('unexpected contract'));
