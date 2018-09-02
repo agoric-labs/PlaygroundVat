edd3e652b948c635d02d2fe5300a4fd8d53babb4
Brian Warner <warner@lothar.com>
2018-09-02 11:09:08 -0700

    add small test of Nat
    just to make sure it's really being provided by SES

---
+++ test/test-nat.js
@@ -0,0 +1,26 @@
+import { test } from 'tape-promise/tape';
+import { confineVatSource, makeRealm, buildVat, bundleCode } from '../src/main';
+import SES from 'ses';
+
+function s1() {
+  exports.foo = (a) => {
+    return Nat(a);
+  };
+}
+
+
+function funcToSource(f) {
+  let code = `${f}`;
+  code = code.replace(/^function .* {/, '');
+  code = code.replace(/}$/, '');
+  return code;
+}
+
+test('Nat', (t) => {
+  const s = SES.makeSESRootRealm();
+  const s1code = funcToSource(s1);
+  const n = confineVatSource(s, funcToSource(s1)).foo;
+  t.equal(n(2), 2);
+  t.end();
+});
+
