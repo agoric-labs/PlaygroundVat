4c7d45b3fee058b05d9e933ac9c18660d7148921
Kate Sills <katelynsills@gmail.com>
2019-03-25 17:32:58 -0700

    make sure AgoricBot is up to date before running npm audit fix, make sure branch doesn't already exist

--- scripts/npm-audit-fix.sh
+++ scripts/npm-audit-fix.sh
@@ -1,12 +1,19 @@
 git clone https://github.com/AgoricBot/PlaygroundVat.git
 cd PlaygroundVat
 git remote add upstream https://github.com/Agoric/PlaygroundVat.git
+git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/PlaygroundVat.git
 git fetch upstream
 git checkout master
 git rebase upstream/master
 git config user.email "kate+agoricbot@agoric.com"
 git config user.name "AgoricBot"
+git config --global hub.protocol https
 hub push origin master
+
+if git ls-remote --heads --exit-code origin npm-audit-fix ; then
+  git push --delete origin npm-audit-fix
+fi
+
 git checkout -b npm-audit-fix
 
 if npm audit ; then
@@ -19,7 +26,6 @@ fi
 if [ "$files_changed" = true ] ; then
   git add . 
   git commit -m "results of running npm audit fix"
-  git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/PlaygroundVat.git
   git push origin npm-audit-fix
   hub pull-request --no-edit --base Agoric/PlaygroundVat:master
 fi
